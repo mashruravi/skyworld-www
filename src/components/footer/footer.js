2054551d49b353c80a99360837ab2a98f0dbfe87
Ravi Suresh Mashru <mashru.ravi@gmail.com>
2019-07-23 07:37:18 +0530

    add contact details

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,6 +7,6 @@ export default function Footer() {
     <div>
       Created and Mainted by <a href='mailto:mashru.ravi@gmail.com'>Ravi Suresh Mashru</a>
     </div>
-    <div>Icons made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a>, <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
   </div>;
 }
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,15 +1,84 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types'
+
+import location from '../../assets/icons/013-earth.svg';
+import mobile from '../../assets/icons/037-tablet.svg';
+import phone from '../../assets/icons/034-phone.svg';
+import whatsapp from '../../assets/icons/006-call.svg';
+import facebook from '../../assets/icons/012-facebook.svg';
+import email from '../../assets/icons/020-mail.svg';
+
+
+import './contact.scss';
 
 export class Contact extends Component {
-  // static propTypes = {
-
-  // }
-
   render() {
     return (
       <div>
-        <h1>Contact Us</h1>
+        <div className="pagewellStyle">
+          <div className="locationImage" />
+
+          <div className="contact-details">
+            <div className="cd-row">
+              <div className="cd-icon">
+                <img src={location} alt="location" />
+              </div>
+              <div className="cd-text">
+                <p>Plot No. 91/97, Anik Building,</p>
+                <p>1st Floor, Suite 3, 7th Street</p>
+                <p>Industrial Area</p>
+                P.O. Box 2801, Kampala (Uganda)
+              </div>
+            </div>
+
+            <div className="cd-row">
+              <div className="cd-icon">
+                <img src={mobile} alt="mobile" />
+              </div>
+              <div className="cd-text">
+                <p>+256 787 815 725</p>
+              </div>
+            </div>
+
+            <div className="cd-row">
+              <div className="cd-icon">
+                <img src={phone} alt="phone" />
+              </div>
+              <div className="cd-text">
+                <p>+256 414 670 800</p>
+              </div>
+            </div>
+
+            <div className="cd-row">
+              <div className="cd-icon">
+                <img src={whatsapp} alt="whatsapp" />
+              </div>
+              <div className="cd-text">
+                <p>+256 759 990 446</p>
+                <p>+256 752 352 177</p>
+              </div>
+            </div>
+
+            <div className="cd-row">
+              <div className="cd-icon">
+                <img src={facebook} alt="facebook" />
+              </div>
+              <div className="cd-text">
+                <p>Skyworld Travels</p>
+              </div>
+            </div>
+
+            <div className="cd-row">
+              <div className="cd-icon">
+                <img src={email} alt="email" />
+              </div>
+              <div className="cd-text">
+                <p>
+                  <a href="mailto:skyworldug@gmail.com">skyworldug@gmail.com</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
