f7014ef1efcc3d54f8174a7fcb20a5b83eb5419f
I322118 <ravi.suresh.mashru@sap.com>
2019-07-23 16:43:37 +0530

    Updates

---
+++ precache-manifest.e185bc78775df2c9d70335639217f0cb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ddbe6d296dedaa5fbf24160415c877",
+    "url": "/skyworld-www/index.html"
+  },
+  {
+    "revision": "89f9e8aef1d7d53e48e6",
+    "url": "/skyworld-www/static/css/main.57d98011.chunk.css"
+  },
+  {
+    "revision": "294bef1bd38f664c38e7",
+    "url": "/skyworld-www/static/js/2.5d9f3cea.chunk.js"
+  },
+  {
+    "revision": "89f9e8aef1d7d53e48e6",
+    "url": "/skyworld-www/static/js/main.2e659a13.chunk.js"
+  },
+  {
+    "revision": "a8e356cfb91d473c4493",
+    "url": "/skyworld-www/static/js/runtime~main.ed9bb054.js"
+  },
+  {
+    "revision": "2df101e39c130da795c58d8ab36b9646",
+    "url": "/skyworld-www/static/media/003-boarding pass.2df101e3.svg"
+  },
+  {
+    "revision": "4bb529ce6593405ac2cdbf3428bb9ad1",
+    "url": "/skyworld-www/static/media/006-call.4bb529ce.svg"
+  },
+  {
+    "revision": "b4804016ce015f2bcb09505ebf0957ec",
+    "url": "/skyworld-www/static/media/012-facebook.b4804016.svg"
+  },
+  {
+    "revision": "cdeb154132b5fb93569cb7d7613a88a2",
+    "url": "/skyworld-www/static/media/013-earth.cdeb1541.svg"
+  },
+  {
+    "revision": "f09fca9af521f6897d4d109bf4079db0",
+    "url": "/skyworld-www/static/media/013-plane.f09fca9a.svg"
+  },
+  {
+    "revision": "ca2a593d585e41a360120fc9d8d2fa43",
+    "url": "/skyworld-www/static/media/017-travel.ca2a593d.svg"
+  },
+  {
+    "revision": "b428cf5e0d84f71a5092e3ea410fd951",
+    "url": "/skyworld-www/static/media/020-mail.b428cf5e.svg"
+  },
+  {
+    "revision": "63f288f8f064a0d475758841fc0a23a4",
+    "url": "/skyworld-www/static/media/034-phone.63f288f8.svg"
+  },
+  {
+    "revision": "fd86afa5c0bf94e339c724876e07a159",
+    "url": "/skyworld-www/static/media/037-tablet.fd86afa5.svg"
+  },
+  {
+    "revision": "85dd2fdb6227dff158062c3c20039034",
+    "url": "/skyworld-www/static/media/logo.85dd2fdb.jpeg"
+  },
+  {
+    "revision": "c066a16636926e083852c710759191b2",
+    "url": "/skyworld-www/static/media/paresh.c066a166.jpg"
+  },
+  {
+    "revision": "956ee67d999e153bd99cc66edf2be4b1",
+    "url": "/skyworld-www/static/media/ugflag1.956ee67d.jpg"
+  }
+]);
