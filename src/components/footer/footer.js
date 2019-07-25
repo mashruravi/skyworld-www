eddefe1e5196fba475ee3d8347891f3cc80de350
Ravi Suresh Mashru <mashru.ravi@gmail.com>
2019-07-25 20:24:07 +0530

    fix typo

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ import './footer.scss';
 export default function Footer() {
   return <div className='footer-container'>
     <div>
-      Created and Mainted by <a href='mailto:mashru.ravi@gmail.com'>Ravi Suresh Mashru</a>
+      Created and Maintained by <a href='mailto:mashru.ravi@gmail.com'>Ravi Suresh Mashru</a>
     </div>
     <div>Icons made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a>, <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a></div>
   </div>;
